ee0671ea356605f123027f05ec62cc4b18cb0da0
Yusuf <121329421+yssfklc@users.noreply.github.com>
2024-01-30 21:32:40 +0300

    add langchain decision agent(ReAct)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain import hub
-from langchain.agents import AgentExecutor
+from langchain.agents import AgentExecutor, initialize_agent
+from langchain_core.tools import Tool
 from langchain_experimental.tools import PythonREPLTool
 from langchain.agents import create_openai_functions_agent
 from langchain.agents.agent_types import AgentType
@@ -23,9 +24,9 @@ def main():
     """
     base_prompt = hub.pull("langchain-ai/openai-functions-template")
     prompt = base_prompt.partial(instructions=instructions)
-    agent = create_openai_functions_agent(ChatOpenAI(temperature=0), tools, prompt)
+    agent_openai = create_openai_functions_agent(ChatOpenAI(temperature=0), tools, prompt)
     agent_executor = AgentExecutor(
-        agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
+        agent=agent_openai, tools=tools, verbose=True, handle_parsing_errors=True
     )
     # agent_executor.invoke(
     #     {
@@ -38,10 +39,42 @@ def main():
         "episode_info.csv",
         verbose=True,
         agent_type=AgentType.OPENAI_FUNCTIONS,
+        handle_parsing_errors=True
     )
 
-    agent_csv.invoke({"input":"Which writer wrote the most episodes? How many episodes did he write"})
+    # agent_csv.invoke({"input":"Which writer wrote the most episodes? How many episodes did he write"})
+    grand_agent = initialize_agent(
+        tools=[
+            Tool(
+                name="PythonAgent",
+                func=agent_executor.invoke,
+                description="Useful when you need to transform natural language and write from it python and execute the python code, "
+                            "returning the results of the code execution, "
+                            "DO NOT SEND PYTHON CODE TO THIS TOOL"
+            ),
+            Tool(
+                name="CSVAgent",
+                func=agent_csv.invoke,
+                description="Useful when you need to answer question over episode_info.csv file, "
+                            "takes an input the entire question and returns the answer after running pandas calculations."
+            ),
+        ],
+        llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613"),
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        verbose=True,
+    )
+    # grand_agent.invoke(
+    #      {
+    #          "input": """Generate and save in current working directory 1 Qr codes
+    #          that point to https://yssfklc.netlify.app/ , you have qrcode package installed already"""
+    #      }
+    # )
 
+    grand_agent.invoke(
+        {
+            "input": """Which writer wrote the most episodes? How many episodes did he write"""
+        }
+    )
 
 if __name__ == "__main__":
 
